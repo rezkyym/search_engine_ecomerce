7e2fae7d1612c1bf45a6268aed8fb49aa084cb41
Rezky <rezkym@student.telkomuniversity.ac.id>
2024-08-08 17:39:28 +0700

    feat(add-theme): add mazer laravel theme

---
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -0,0 +1,6 @@
+<x-mazer-sidebar :href="route('dashboard')" logo="asset('static/images/logo/logo.png')">
+    <x-mazer-sidebar-item icon="bi bi-grid-fill" :link="route('dashboard')" name="Dashboard" />
+    <x-mazer-sidebar-item icon="bi bi-stack" :link="route('dashboard')" name="Components">
+        <x-mazer-sidebar-subitem :link="route('component.accordion')" name="Accordion" :active="false"/>
+    </x-mazer-sidebar-item>
+</x-mazer-sidebar>
